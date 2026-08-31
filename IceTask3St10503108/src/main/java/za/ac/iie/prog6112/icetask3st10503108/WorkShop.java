4c2f06a71f620e1303b45bca8019b7bc48a5412c
vincentjonas065 <vincentjonas065@gmail.com>
2026-08-31 14:09:47 +0200

    Update WorkShop.java

--- IceTask3St10503108/src/main/java/za/ac/iie/prog6112/icetask3st10503108/WorkShop.java
+++ IceTask3St10503108/src/main/java/za/ac/iie/prog6112/icetask3st10503108/WorkShop.java
@@ -8,6 +8,33 @@ package za.ac.iie.prog6112.icetask3st10503108;
  *
  * @author Student
  */
-public class WorkShop {
-    
+
+
+/**
+ * Abstract class implementing IWorkshop to encapsulate shared workshop fields and behavior.
+ */
+public abstract class Workshop implements IWorkshop {
+    // Private fields for data hiding / encapsulation
+    private String workshopName;
+    private int bookings;
+
+    // Constructor accepts a WorkshopModel object and initializes private fields
+    public Workshop(WorkshopModel model) {
+        this.workshopName = model.WorkshopName;
+        this.bookings = model.Bookings;
+    }
+
+    @Override
+    public String GetWorkshopName() {
+        return workshopName;
+    }
+
+    @Override
+    public int GetBookings() {
+        return bookings;
+    }
+
+    // Abstract method to be overridden by subclasses to demonstrate dynamic method binding
+    public abstract String GetWorkshopMode();
 }
+
