da91556380cb86995fd4c8f372904f8aef8111df
vincentjonas065 <vincentjonas065@gmail.com>
2026-08-31 14:07:09 +0200

    Update WorkShopModel.java

--- IceTask3St10503108/src/main/java/za/ac/iie/prog6112/icetask3st10503108/WorkShopModel.java
+++ IceTask3St10503108/src/main/java/za/ac/iie/prog6112/icetask3st10503108/WorkShopModel.java
@@ -8,6 +8,21 @@ package za.ac.iie.prog6112.icetask3st10503108;
  *
  * @author Student
  */
-public class WorkShopModel {
+public class WorkShopModel { 
+package workshopmanagement;
+
+/**
+ * Model class used to store and populate raw workshop data.
+ */
+    public String WorkshopName;
+    public int Bookings;
+
+    // Constructor to easily initialize model fields
+    public WorkshopModel(String workshopName, int bookings) {
+        this.WorkshopName = workshopName;
+        this.Bookings = bookings;
+    }
+}
+
     
 }
