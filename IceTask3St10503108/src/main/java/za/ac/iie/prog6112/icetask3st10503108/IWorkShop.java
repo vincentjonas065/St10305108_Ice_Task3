ca99a53d08be27ce5217ec128188f89dafd5715f
vincentjonas065 <vincentjonas065@gmail.com>
2026-09-01 14:26:25 +0200

    IWorkshop
    Add/fix IWorkshop.java with GetWorkshopName() and GetBookings()

--- IceTask3St10503108/src/main/java/za/ac/iie/prog6112/icetask3st10503108/IWorkShop.java
+++ IceTask3St10503108/src/main/java/za/ac/iie/prog6112/icetask3st10503108/IWorkShop.java
@@ -1,20 +1,12 @@
 package za.ac.iie.prog6112.icetask3st10503108;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
  * @author Student
  */
+public interface IWorkshop {
 
-
-public interface IWorkShop {
     String GetWorkshopName();
+
     int GetBookings();
 }
-
-    
-
