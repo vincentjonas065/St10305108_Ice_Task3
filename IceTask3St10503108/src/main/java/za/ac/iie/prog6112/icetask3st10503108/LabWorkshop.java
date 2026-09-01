9f68c43a8cf86aed5c8e3710bc0d72be67d58ee5
vincentjonas065 <vincentjonas065@gmail.com>
2026-09-01 14:27:47 +0200

    LabWorkshop
    fix Workshop.java, inheritance from IWorkshop, fields, constructor and getters

--- IceTask3St10503108/src/main/java/za/ac/iie/prog6112/icetask3st10503108/LabWorkshop.java
+++ IceTask3St10503108/src/main/java/za/ac/iie/prog6112/icetask3st10503108/LabWorkshop.java
@@ -1,17 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package za.ac.iie.prog6112.icetask3st10503108;
 
 /**
- * Subclass representing a workshop delivered in a computer laboratory.
- * @author Student
+ *
+ * @author Vincent
  */
-public class LabWorkshop extends WorkShop {
+public class LabWorkshop extends Workshop {
 
-    public LabWorkshop(WorkShopModel model) {
-        super(model); // Calls the superclass constructor
+    public LabWorkshop(WorkshopModel model) {
+        super(model);
     }
 
     @Override
