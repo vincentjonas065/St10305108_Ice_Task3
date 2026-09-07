efda735cb52ffc3f28566058eb294e5b902c1ca9
vincentjonas065 <vincentjonas065@gmail.com>
2026-09-07 19:35:57 +0200

    Refactor Workshop class variable names for consistency

--- IceTask3St10503108/src/main/java/za/ac/iie/prog6112/icetask3st10503108/Workshop.java
+++ IceTask3St10503108/src/main/java/za/ac/iie/prog6112/icetask3st10503108/Workshop.java
@@ -11,31 +11,23 @@ package za.ac.iie.prog6112.icetask3st10503108;
  */
 public abstract class Workshop implements IWorkshop {
 
-    private final String WorkshopName;
-    private final int Bookings;
+    private String workshopName;
+    private int bookings;
 
     public Workshop(WorkshopModel model) {
-        this.WorkshopName = model.WorkshopName;
-        this.Bookings = model.Bookings;
+        this.workshopName = model.WorkshopName;
+        this.bookings = model.Bookings;
     }
 
     @Override
     public String GetWorkshopName() {
-        return WorkshopName;
+        return workshopName;
     }
 
     @Override
     public int GetBookings() {
-        return Bookings;
+        return bookings;
     }
 
     public abstract String GetWorkshopMode();
-
-    public int getBookings() {
-        return Bookings;
-    }
-
-    public String getWorkshopName() {
-        return WorkshopName;
-    }
 }
